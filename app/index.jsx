87f4805dd9c2626fa7851b6c629efa65e7119e76
TotalRage <=>
2025-11-24 00:05:47 -0500

    v1.0.0

--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,22 @@ import {StyleSheet, Text, View} from "react-native";
 
 const Home = () => {
     return (
-        <View>
-            <Text>Hi :P</Text>
+        <View style={styles.container}>
+            <Text>Developed by Infinity Studios</Text>
         </View>
     )
 }
 
 export default Home
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffffff'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+});
