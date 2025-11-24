0a58265017c0f6667139bc380d6f716a24ea3bf1
TotalRage <=>
2025-11-23 22:10:04 -0500

    v1.0.0

---
+++ App.js
@@ -0,0 +1,21 @@
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I am Batman</Text>
+      <Image source={require('./assets/bomb-touch-icon.png')}/>
+      <StatusBar style="auto"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5a5a5aff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
